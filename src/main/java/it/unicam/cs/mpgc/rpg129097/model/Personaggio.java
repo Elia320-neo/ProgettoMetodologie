6fde12b917341b5cdfa0b2976037e65f1deffc3f
Elia Magini <elia.magini@studenti.unicam.it>
2026-06-28 10:31:13 +0200

    First Commit
    add Personaggio, config .gitignore

---
+++ src/main/java/it/unicam/cs/mpgc/rpg129097/model/Personaggio.java
@@ -0,0 +1,26 @@
+package it.unicam.cs.mpgc.rpg129097.model;
+
+
+public abstract class Personaggio {
+    protected String nome;
+    protected boolean vivo;
+
+
+    protected Personaggio(String nome){
+        if(nome == null) throw new NullPointerException("nome non può essere Null");
+        this.nome = nome;
+        this.vivo = true;
+    }
+
+
+    public String getNome() {return this.nome;}
+
+    public boolean isVivo() {return this.vivo;}
+
+    public void uccidi() {this.vivo=false;}
+
+
+    public abstract void presentati();
+
+
+}
