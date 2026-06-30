63201da4b517ff3508ca36091ec058422e136949
Elia Magini <elia.magini@studenti.unicam.it>
2026-06-30 11:13:12 +0200

    add constructor , getDescrizione(), getEsito() in Fatto

--- src/main/java/it/unicam/cs/mpgc/rpg129097/model/Fatto.java
+++ src/main/java/it/unicam/cs/mpgc/rpg129097/model/Fatto.java
@@ -1,4 +1,21 @@
 package it.unicam.cs.mpgc.rpg129097.model;
 
-public class Fatto {
+import it.unicam.cs.mpgc.rpg129097.interfaces.Proposizione;
+
+public class Fatto implements Proposizione {
+    private final String descrizione;
+    private final boolean esito;
+
+    public Fatto(String descrizione, boolean esito) {
+        if(descrizione == null) throw new NullPointerException("descrizione non può essere null");
+        this.descrizione = descrizione;
+        this.esito = esito;
+    }
+
+    @Override
+    public String getDescrizione() { return descrizione; }
+
+    @Override
+    public boolean getEsito() { return esito; }
 }
+
